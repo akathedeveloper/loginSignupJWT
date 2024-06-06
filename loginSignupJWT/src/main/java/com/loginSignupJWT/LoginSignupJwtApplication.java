00cb52793da4157a0e8097560aa52894c25d9254
Adhiraj Singh <114748865+akathedeveloper@users.noreply.github.com>
2024-06-06 14:13:41 +0530

    Initail Project

---
+++ loginSignupJWT/src/main/java/com/loginSignupJWT/LoginSignupJwtApplication.java
@@ -0,0 +1,13 @@
+package com.loginSignupJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSignupJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSignupJwtApplication.class, args);
+	}
+
+}
---
+++ loginSignupJWT/src/test/java/com/loginSignupJWT/LoginSignupJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginSignupJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSignupJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
